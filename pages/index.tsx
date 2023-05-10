773f3f8a36908545c001b5ff59a151bfe8e3fcbb
‘react-guru007’ <passionguy211@gmail.com>
2023-05-09 21:04:53 -0400

    didnt work trying this

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home({ data }: HomeProps) {
 
   useEffect(() => {
     setSuggestionData(data)
-  },[])
+  },[openNewFeedback])
 
   return (
     <div className="container">
